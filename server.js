9262133d40eb3f7d26442bf7f9da0da67677650e
bendevman <bendevman@gmail.com>
2022-10-12 11:42:25 +0300

    basic server setitngs

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 
+app.set('view engine','ejs')
 
+app.get("/",(req,res)=>{
+  console.log('hi')
+  res.render('index')
+})
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
